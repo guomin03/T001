b21591b8cb1f6673911e95237896d21ca83a2de5
guomin03 <18117850203@163.com>
2017-09-22 19:28:58 +0800

    Initial commit 啥子呀。看不懂

---
+++ app/src/main/java/com/example/dell/myapplication/githubTest.java
@@ -0,0 +1,11 @@
+package com.example.dell.myapplication;
+
+/**
+ * Created by dell on 2017/9/22.
+ */
+
+public class githubTest {
+    public static void test(){
+        System.out.println(123);
+    }
+}
